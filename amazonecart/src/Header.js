eca977ab949b8582568d1235e8895990a8238e0c
yaya <anbesie.yg@gmail.com>
2020-09-08 19:10:20 -0700

    the header part is finished

--- amazonecart/src/Header.js
+++ amazonecart/src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import SearchIcon from '@material-ui/icons/Search';
 function Header() {
   return (
     <div className='header'>
-      <img  className='header_logo'src='https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FG%2F01%2Fgc%2Fdesigns%2Flivepreview%2Famazon_drkblue_noto_printfold_v2016_us-main._CB468920742_.png&imgrefurl=https%3A%2F%2Fwww.amazon.com%2FAmazon-com-Gift-Cards-Print-Home%2Fdp%2FBT00DDC7BK&tbnid=x9QcBoHHhmTdcM&vet=12ahUKEwj4mPze59rrAhVBgZ4KHa6rCVUQMygAegUIARDOAQ..i&docid=yzrh6PJyufZgVM&w=500&h=380&q=amazon%20logo&ved=2ahUKEwj4mPze59rrAhVBgZ4KHa6rCVUQMygAegUIARDOAQ' alt="img"></img>
+      <img  className='header_logo'src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' alt="img"></img>
       <div className='header_search'>
-        <input className='header_searchIn'></input>
+        <input className='header_searchInput' type='text'/>
+        <SearchIcon className='header_searchIcon'/>
       </div>
       <div className='header_nav'>
         <div className='header_option'>
