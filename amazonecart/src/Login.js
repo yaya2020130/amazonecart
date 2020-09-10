08c55659e8a247b867be17a1f7c5256544928426
yaya <anbesie.yg@gmail.com>
2020-09-10 01:56:24 -0700

    login auth is created

--- amazonecart/src/Login.js
+++ amazonecart/src/Login.js
@@ -1,19 +1,38 @@
 import React ,{useState}from 'react'
-import {Link} from 'react-router-dom'
+import {Link,useHistory} from 'react-router-dom'
 import './Login.css'
+import {auth } from "./firebase"
 function Login() {
+  const history=useHistory()
   const [email,setEmail]=useState('')
   const [password,setPassword]=useState('')
+
   const signIn=e=>{
     e.preventDefault()
     //firebase login
+auth
+.signInWithEmailAndPassword(email,password)
+  .then(auth =>{
+    history.push('/')
 
-  }
+  })
+  .catch(error=>alert(error.message))
+}
   const register = e=>{
     e.preventDefault()
-  
      //firebase register
-  }
+
+     
+auth
+.createUserWithEmailAndPassword(email,password)
+ .then((auth)=>{
+   if(auth){
+     history.push('/')
+   }
+   
+ }) 
+ .catch(error=>alert(error.message))
+}
 
   return (
     <div className='login'>
--- amazonecart/src/firebase.js
+++ amazonecart/src/firebase.js
@@ -9,3 +9,7 @@ const firebaseConfig = {
   appId: "1:295555374275:web:2523be962d725e22409676",
   measurementId: "G-B4V3YG1D8S"
 };
+const firebaseApp=firebase.initializeApp(firebaseConfig);
+const db=firebase.firestore();
+const auth= firebase.auth();
+export {db,auth};
